562e687429d7678b0f9894b37364b7a29b1598a1
KungrcKMK <kungrckmk@appstock.local>
2026-07-27 18:25:34 +0700

    auto: sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Cache static assets สำหรับใช้งาน offline บางส่วน
 // =====================================================
 
-const CACHE_NAME = "appstock-v16";
+const CACHE_NAME = "appstock-v17";
 
 // ไฟล์ที่ cache ไว้ใช้ offline
 const STATIC_ASSETS = [
